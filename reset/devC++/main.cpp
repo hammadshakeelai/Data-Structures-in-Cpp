c441f701fd3856672a293d0f68d798c74f50d525
hammadshakeelai <hammadshakeel61@gmail.com>
2025-09-20 19:27:17 +0500

    Update main.cpp

--- reset/devC++/main.cpp
+++ reset/devC++/main.cpp
@@ -1,12 +1,21 @@
-
 #include<iostream>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 int main(){
-    int num;
-    cin>>num;
+	int a;
+	cin>>a;
+	auto start = high_resolution_clock::now();   // start time
+	//---------------------------------------------------------------
+    int num=0;
+    num=a;
     cout<<num%10;
     cout<<(num/10)%10;
     cout<<(num/100)%10;
-    
+    //---------------------------------------------------------------
+    auto end = high_resolution_clock::now();     // end time
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    cout << "Execution time: " << duration.count() << " ms\n";
     return 0;
 }
