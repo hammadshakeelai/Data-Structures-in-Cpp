d7da9a711a2222e5725e4a00db49351fb64376d8
hammadshakeelai <hammadshakeel61@gmail.com>
2025-10-16 21:50:42 +0500

    Update main.cpp

--- devC++/main.cpp
+++ devC++/main.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
-#include <chrono>
 #include <fstream>
 using namespace std;
-using namespace std::chrono;
-// ips.txt
-// 192.168.1.10,10.0.0.5,172.16.0.1,203.0.113.5,198.51.100.23,8.8.8.8,8.8.4.4,192.0.2.45,10.10.10.10,172.20.14.2,52.214.10.7,34.102.136.180,185.199.108.153,13.35.66.1,44.240.20.10
 class Node {
 	public:
 		string ip;
@@ -101,47 +97,37 @@ void split(string text,string array[]) {
 	}
 	array[pointer] = word;
 }
-int numofips(string text){
-	int counter = 0;
-	for(char ch:text){
+int main(){
+	//---------------------------------------------------------------
+	DoublyLinkedList *ipAddressList = new DoublyLinkedList;
+	ifstream file("ips.txt");
+	string line;
+	getline(file, line);
+	int counter = 1;
+	for(char ch:line){
 		if (ch==','){
 			counter++;
 		}
 	}
-	return ++counter;
-}
-int main(){
-	auto start = high_resolution_clock::now();   // start time
-	//---------------------------------------------------------------
-	ifstream file("ips.txt");
-	//<-- open file -->
-//	if (!file) { // wrote to test if my file was connected 
-//		cerr << "Error opening file." << endl;
-//	}
-	DoublyLinkedList *ipAddressList = new DoublyLinkedList;
-	string line;
-	getline(file, line);
-//	cout << line;
-	string arr[numofips(line)];
-	split(line,arr);
-	for(string ip : arr){
-		ipAddressList->insertAtTheBeginning(ip);
+	string word = "";
+	int pointer = 0;
+	for(char ch : line){
+		if(ch==','){
+			if (word != ""){
+				ipAddressList->insertAtTheBeginning(word);
+				pointer++;
+				word="";
+			}
+		}
+		else{
+			word = word + ch;
+		}
 	}
+	ipAddressList->insertAtTheBeginning(word);
 	ipAddressList->Display();
 	ipAddressList->Display_Reverse();
-	
 	delete ipAddressList;
-	//<-- close file -->
 	file.close();
-//	DoublyLinkedList *ipAddressList = new DoublyLinkedList;
-//	ipAddressList->Display();
-//	string ip;
-//	file.get(ch)
-//	ipAddressList.insertAtTheBeginning(ip);
     //---------------------------------------------------------------
-    auto end = high_resolution_clock::now();     // end time
-    auto duration = duration_cast<milliseconds>(end - start);
-
-    cout << "\nExecution time: " << duration.count() << " ms\n";
     return 0;
 }
