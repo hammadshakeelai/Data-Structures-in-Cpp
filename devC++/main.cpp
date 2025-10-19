735c4481243a433043e022df1056b4da7ed1c405
hammadshakeelai <hammadshakeel61@gmail.com>
2025-10-19 12:00:11 +0500

    Update main.cpp

--- devC++/main.cpp
+++ devC++/main.cpp
@@ -3,28 +3,27 @@
 using namespace std;
 class Node {
 	public:
-		string ip;
+		int data;
 		Node* next;
-		Node* prev;
-		Node(string iP) : ip(iP), prev(nullptr), next(nullptr) {}
+		Node(int value) : data(value), next(nullptr) {}
 };
-class DoublyLinkedList{
+class SinglyLinkedList{
 	public:
 		Node *head;
 		Node *tail;
-		DoublyLinkedList(){
+		SinglyLinkedList(){
 			cout << "Constructor called, list initialized." << endl;
 			head = nullptr;
 			tail = nullptr;
 		}
-		~DoublyLinkedList(){
+		~SinglyLinkedList(){
 			Node* temp = head;
 			while(head != nullptr){
 				head = head->next;
 				delete temp;
 				temp = head;
 			}
-			cout << "Destructor called, memory freed." << endl;
+			cout << "\nDestructor called, memory freed." << endl;
 		}
 		void Display(){
 			Node* curr = head;
@@ -33,7 +32,7 @@ class DoublyLinkedList{
 				return;
 			}
 			while(curr != nullptr){
-				cout << curr->ip;
+				cout << curr->data;
 				if(curr->next != nullptr){
 					cout << " -> ";
 				}
@@ -41,19 +40,15 @@ class DoublyLinkedList{
 			}
 			cout << endl;
 		}
-		void insertAtTheBeginning(string value){
+		void insertAtTheBeginning(int value){
 			Node* newNode = new Node(value);
-			if(head != nullptr){
-				head->prev = newNode;
-			} 
 			newNode->next = head;
-			
 			if(head == nullptr){
 				tail = newNode;
 			}
 			head = newNode;
 		}
-		void insertAtEnd(string value){
+		void insertAtEnd(int value){
 			Node* newNode = new Node(value);
 			if(tail == nullptr){
 				head = newNode;
@@ -61,23 +56,129 @@ class DoublyLinkedList{
 				return;
 			}
 			tail->next = newNode;
-			newNode->prev = tail;
 			tail = newNode;
 		}
-		void Display_Reverse(){
-			Node* curr = tail;
-			if(tail==nullptr){
+		void DisplayReverse(){
+			if(head==nullptr){
 				cout << "List is Empty." << endl;
 				return;
 			}
-			while(curr != nullptr){
-				cout << curr->ip;
-				if(curr->prev != nullptr){
-					cout << " <- ";
+			int counter = 1;
+			Node* curr = head;
+			while(curr->next != tail){
+				curr = curr->next;
+				counter++;
+			}
+			counter++;
+			cout << curr->next->data;
+			for(int i=(counter-1);i>=1;i--){
+				curr = head;
+				for(int j=1;j<i;j++){
+					curr = curr->next;
 				}
-				curr = curr->prev;
+				cout << " <- " << curr->data;
 			}
 			cout << endl;
+		}
+		void DisplayReverse_without_tail(){// without tail
+			if(head==nullptr){
+				cout << "List is Empty." << endl;
+				return;
+			}
+			int counter = 0;
+			Node* curr = head;
+			while(curr->next != nullptr){
+				curr = curr->next;
+				counter++;
+			}
+			cout << curr->data;
+			for(int i=counter;i>=1;i--){
+				curr = head;
+				for(int j=1;j<i;j++){
+					curr = curr->next;
+				}
+				cout << " <- " << curr->data;
+			}
+			cout << endl;
+		}
+		void ReverseTheList(){// without tail
+			if(head==nullptr){
+				cout << "List is Empty." << endl;
+				return;
+			}
+			if(head == tail){// head->next == nullptr
+				cout << "List Reversed." << endl;
+			}
+			int counter = 1;
+//			Node* temp_ = head;
+//			Node* _temp = head;
+//			Node* _curr = head;
+			Node* pBehindCurrAtStart;
+			Node* currAtStart;
+			Node* pBehindCurrAtEnd;
+			Node* currAtEnd = head;
+			while(currAtEnd->next != nullptr){
+				currAtEnd = currAtEnd->next;
+				counter++;
+			}
+			cout << "list size: "<< counter << endl;
+			int iteration = 0;
+			for(int i=counter;i>0;i--){
+				pBehindCurrAtStart = head;
+				pBehindCurrAtEnd = head;
+				currAtStart = head;
+				currAtEnd = head;
+				for(int j=1;j<i;j++){
+					pBehindCurrAtEnd = currAtEnd;
+					currAtEnd = currAtEnd->next;
+				}
+				for(int j=(counter-i);j>0;j--){
+					pBehindCurrAtStart = currAtStart;
+					currAtStart = currAtStart->next;
+				}
+				if(currAtStart == currAtEnd || currAtStart->next == currAtEnd){
+					break;
+				}
+				cout << "behindStartcurr: " << pBehindCurrAtStart->data << endl;
+				cout << "Startcurr: " << currAtStart->data << endl;
+				cout << "behindEndcurr: " << pBehindCurrAtEnd->data << endl;
+				cout << "Endcurr: " << currAtEnd->data << endl;
+				tail = currAtStart;
+				Node* temp = currAtEnd->next;
+				currAtEnd->next = currAtStart->next;
+				currAtStart->next = temp;
+				pBehindCurrAtEnd->next = currAtStart;
+				if (iteration == 0){
+					head = currAtEnd;
+				}else{
+					pBehindCurrAtStart->next = currAtEnd;
+				}
+				iteration++;
+				
+				
+			}
+//			cout << curr_->data << endl;
+//			head = curr_;
+//			tail = _curr;
+//			curr_ = _curr->next;
+//			_curr->next = nullptr;
+//			temp_->next = _curr;
+			
+			
+//			cout << curr->data;
+//			for(int i=counter;i>=1;i--){
+//				curr_ = head;
+//				for(int m = (counter+1-i);m>0;m--){
+//					_temp = _curr;
+//					_curr = _curr->next;
+//				}
+//				for(int j=1;j<i;j++){
+//					temp_ = curr_;
+//					curr_ = curr_->next;
+//				}
+//				
+//				cout << " <- " << curr->data;
+//			}
 		}
 };
 void split(string text,string array[]) {
@@ -99,35 +200,20 @@ void split(string text,string array[]) {
 }
 int main(){
 	//---------------------------------------------------------------
-	DoublyLinkedList *ipAddressList = new DoublyLinkedList;
-	ifstream file("ips.txt");
-	string line;
-	getline(file, line);
-	int counter = 1;
-	for(char ch:line){
-		if (ch==','){
-			counter++;
-		}
-	}
-	string word = "";
-	int pointer = 0;
-	for(char ch : line){
-		if(ch==','){
-			if (word != ""){
-				ipAddressList->insertAtTheBeginning(word);
-				pointer++;
-				word="";
-			}
-		}
-		else{
-			word = word + ch;
-		}
-	}
-	ipAddressList->insertAtTheBeginning(word);
-	ipAddressList->Display();
-	ipAddressList->Display_Reverse();
-	delete ipAddressList;
-	file.close();
+	SinglyLinkedList l;
+	l.insertAtEnd(1);
+	l.insertAtEnd(2);
+	l.insertAtEnd(3);
+	l.insertAtEnd(4);
+	l.insertAtEnd(5);
+	l.insertAtEnd(6);
+	l.insertAtEnd(7);
+	l.insertAtEnd(8);
+	l.insertAtEnd(9);
+//	l.Display(); // 9 -> 11
+//	l.DisplayReverse_without_tail(); // 11 <- 9
+	l.ReverseTheList();
+	l.Display();
     //---------------------------------------------------------------
     return 0;
 }
