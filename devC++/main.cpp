1687aa3aeded1ab5af1af35ca3926a96f6a360e4
hammadshakeelai <hammadshakeel61@gmail.com>
2025-11-30 23:34:05 +0500

    Update main.cpp

--- devC++/main.cpp
+++ devC++/main.cpp
@@ -1,63 +1,112 @@
 #include<iostream>
 #include <chrono>
+#include <thread>
 using namespace std;
-using namespace std::chrono;
-#define size 10
-// --- <stack>
-class Stack{
-	public:
+class Queue {
+	private:
 		int* arr;
-		int top;
-		int maxSize;
-		Stack(){
-			arr = new int[size];
-			top = -1;
-			maxSize = size;
-		}
-		void Stack_New(int s){
-			top = -1;
-			arr = new int[s];
-			maxSize = size;
-		}
-		void push(int value){(top != maxSize)?(arr[++top] = value):(cout<<"Stack Overflow"<<endl);}
-		int pop(){(top == -1)?(cout<<"Stack Underflow"<<endl;return -1):return arr[top--];}
-		int peek(){(top == -1)?cout<<("Stack Underflow"<<endl;return -1):return arr[top];}
-		void display(){
-			if(top==-1){
-				cout<<"Stack Underflow"<<endl;
-				return
+		int capacity;//max size
+		int frontIdx;
+		int rearIdx;
+		int currentSize;
+		void resize() {
+			int newCapacity = capacity * 2;
+			int* newArr = new int[newCapacity];
+			for (int i = 0; i < currentSize; i++) {
+				newArr[i] = arr[(frontIdx + i) % capacity];
 			}
-			for(int i=0;i<=top;i++){
-				cout<<arr[i]<<" ";
+			delete[] arr;
+			arr = newArr;
+			capacity = newCapacity;
+			frontIdx = 0;
+			rearIdx = currentSize;
+		}
+	public:
+		Queue(int initialCapacity = 4) {
+			capacity = initialCapacity;
+			arr = new int[capacity];
+			frontIdx = 0;
+			rearIdx = 0;
+			currentSize = 0;
+		}
+		~Queue() {
+			delete[] arr;
+		}
+		bool isEmpty() const {
+			if (currentSize == 0) {
+				return true;
 			}
-			cout<<endl;
+			if (currentSize != 0) { // not important
+				return false;
+			}
+		}
+		int size() const {
+			return currentSize;
+		}
+		void enqueue(int value) {
+			if (currentSize == capacity) {
+				resize();
+			}
+			arr[rearIdx] = value;
+			rearIdx = (rearIdx + 1) % capacity;
+			currentSize++;
+		}
+		void dequeue() {
+			if (isEmpty()) {
+				cout << "Queue Underflow! Cannot dequeue from an empty queue.\n";
+				return;
+			}
+			frontIdx = (frontIdx + 1) % capacity;
+			currentSize--;
+		}
+		int front() const {
+			if (isEmpty()) {
+				cout << "Queue is empty. No front element.\n";
+				return -1;
+			}
+			return arr[frontIdx];
+		}
+		void print() const {
+			cout << "Queue elements: ";
+			for(int i = 0;i < currentSize;i++) {
+				int index = (frontIdx + i) % capacity;
+				cout << arr[index] << " ";
+			}
+			cout << "\n";
+		}
+		bool isFull() {
+			if (capacity == currentSize){
+				return true;
+			}
+			return false;
+		}
+		void reload(int &a,int &b) {// not of queue
+			while(!isFull()){
+				this_thread::sleep_for(chrono::microseconds(20000));
+				enqueue(0);
+				cout << b++ << ": ";
+				cout << "Magazine Reloaded.\n";
+				a--;
+			}
+		}
+		void shoot() {
+			this_thread::sleep_for(chrono::microseconds(20000));
+			dequeue();
+			cout << "Bullet Shot.\n";
 		}
 };
-bool areBracketsBalanced(string expr){
-	Stack st;
-	for(char ch : expr){
-		if ( ch == '{' || ch =='[' || ch == '<' || ch == '('){
-			st.push(ch);
-		}
-		else if ( ch == '}' || ch ==']' || ch == '>' || ch == ')'){
-			if (ch == st.peek();)
-				st.pop(ch);
-			else
-				return false;
-		}
-	}
-	if(st.top != -1)
-		return false;
-	return true;
-}
 int main() {
-	auto start = high_resolution_clock::now();   // start time
-	//---------------------------------------------------------------
-	cout<<areBracketsBalanced("{{}}")
-	//---------------------------------------------------------------
-	auto end = high_resolution_clock::now();     // end time
-	auto duration = duration_cast<milliseconds>(end - start);
-
-	cout << "\nExecution time: " << duration.count() << " ms\n";
+	int bullets = 100;
+	Queue *magazine = new Queue(20);
+	int i = 0;
+	while(bullets > 0){
+		
+		magazine->reload(bullets,i);
+		cout << i++ << ": ";
+		magazine->shoot();
+		
+	}
+	cout << "Bullets have ran out.\n";
 	return 0;
+
 }
