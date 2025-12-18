33177b61d79868d860e0e9bb9ce334b3a2a70c80
hammadai <hammadshakeel61@gmail.com>
2025-12-18 23:03:22 +0500

    Update selection sort.cpp

---
+++ Lab Tasks/lab13/selection sort.cpp
@@ -0,0 +1,190 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+//off_t lseek(int fd, off_t offset, int whence);
+using namespace std;
+struct Node {
+    int studentID;
+    string studentName;
+    string DepartmentCode;
+    int Semester;
+    double CGPA;
+    int CeditHoursCompleted;
+    int EnrollmentYear;
+    Node* next;
+    Node(int s_id, string s_n, string s_d_code, int s_sems, double s_cgpa, int s_CRH, int s_en_year) {
+        this->studentID = s_id;
+        this->studentName = s_n;
+        this->DepartmentCode = s_d_code;
+        this->Semester = Semester;
+        this->CGPA = s_cgpa;
+        this->CeditHoursCompleted = s_CRH;
+        this->EnrollmentYear = s_en_year;
+        next = nullptr;
+    }
+};
+class LinkedList {
+public:
+    int Total_Number_of_Students;
+    Node* head;
+public:
+    LinkedList(int t_s) {
+        this->Total_Number_of_Students = t_s;
+        head = nullptr;
+    }
+    ~LinkedList() {
+        while (head != nullptr) {
+            deleteFromBeginning();
+        }
+
+        //        Node* cur = head;
+        //        Node *temp =nullptr;
+        //        while(cur->next != nullptr){
+        //            temp = cur->next;
+        //            delete cur;
+        //            cur = temp;
+        //        }
+
+    }
+    void insertAtBeginning(int s_id, string s_n, string s_d_code, int s_sems, double s_cgpa, int s_CRH, int s_en_year) {
+        Node* temp = new Node(s_id, s_n, s_d_code, s_sems, s_cgpa, s_CRH, s_en_year);
+        temp->next = head;
+        head = temp;
+    }
+    void insertAtEnd(int s_id, string s_n, string s_d_code, int s_sems, double s_cgpa, int s_CRH, int s_en_year) {
+        Node* temp = new Node(s_id, s_n, s_d_code, s_sems, s_cgpa, s_CRH, s_en_year);
+        if (head == nullptr) {
+            head = temp;
+            return;
+        }
+        Node* cur = head;
+        while (cur->next != nullptr) {
+            cur = cur->next;
+        }
+        cur->next = temp;
+    }
+    void insertAtPosition(int s_id, string s_n, string s_d_code, int s_sems, double s_cgpa, int s_CRH, int s_en_year, int position) {
+        if (position == 0) {
+            insertAtBeginning(s_id, s_n, s_d_code, s_sems, s_cgpa, s_CRH, s_en_year);
+            return;
+        }
+        Node* temp = new Node(s_id, s_n, s_d_code, s_sems, s_cgpa, s_CRH, s_en_year);
+        Node* cur = head;
+        for (int i = 0; i < position - 1 && cur != nullptr; i++) {
+            cur = cur->next;
+        }
+        if (cur == nullptr) {
+            cout << "Position out of bounds. Inserting at the end." << endl;
+            insertAtEnd(s_id, s_n, s_d_code, s_sems, s_cgpa, s_CRH, s_en_year);
+            return;
+        }
+        temp->next = cur->next;
+        cur->next = temp;
+    }
+    bool deleteFromBeginning(int* removed = nullptr) {
+        if (head == nullptr) {
+            cout << "List is empty. Cannot delete." << endl;
+            return false;
+        }
+        Node* temp = head;
+        if (removed != nullptr) {
+            *removed = head->studentID;
+        }
+        head = head->next;
+        delete temp;
+        return true;
+    }
+    void deleteFromEnd() {
+        if (head == nullptr) {
+            cout << "List is empty. Cannot delete." << endl;
+            return;
+        }
+        if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node* cur = head;
+        while (cur->next->next != nullptr) {
+            cur = cur->next;
+        }
+        delete cur->next;
+        cur->next = nullptr;
+    }
+    void display() {
+        Node* cur = head;
+        while (cur != nullptr) {
+            cout << "<" << cur->studentID << "> <" << cur->studentName << "> <" << cur->DepartmentCode << "> <" << cur->Semester << "> <" << cur->CGPA << "> <" << cur->CeditHoursCompleted << "> <" << cur->EnrollmentYear << "> " << " ->\n";
+            cur = cur->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+};
+////int arr[]={6,5,3,2,4,1}
+////int temp;
+void selectionsort(int* arr, int n) {
+    int temp, cur;
+    for (int i = 0; i < n - 1; i++) {
+        temp = arr[i];
+        cur = i;
+        for (int j = i + 1; j < n; j++) {
+            if (temp <= arr[j]) {
+                continue;
+            }
+            else {
+                temp = arr[j];
+                cur = j;
+            }
+        }
+        arr[cur] = arr[i];
+        arr[i] = temp;
+    }
+}
+int main() {
+	//cout << "GFV"; testing my failure of a compiler
+     
+    //file opening ceremony
+    ifstream scan("students_data.txt");
+    if (!scan.is_open()) {
+        cerr << "Error: Unable to open file!" << endl;
+        return 1;
+    }
+    //linkedlist scenes
+    int line;
+    scan >> line;
+    //scan.ignore();
+    LinkedList data(line);
+
+    //string test;
+    //if (!std::getline(scan, test))
+    //   cerr << "Error: Failed to read data" << endl;*
+
+    
+
+    int sid, sem, ch, ey; double cgpa; string sname, dcode;
+
+    for (int i = 0; i < 2; i++) {
+        scan >> sid >> sname >> dcode >> sem >> cgpa >> ch >> ey;
+        scan.ignore();
+        cout << sid << " " << sname << " " << dcode << " " << sem << " " << cgpa << " " << ch << " " << ey << endl;
+        data.insertAtEnd(sid, sname, dcode, sem, cgpa, ch, ey);
+    }
+    //data.display();
+    data.display();
+
+	string test,t;
+    std::getline(scan, test);
+	cout << "- " << test << endl;
+    std::getline(scan, t);
+    cout << "- " << test << endl;
+    // 
+    // 
+    //-----
+
+    //if (scan.eof())
+    //    cout << "Reached end of file." << endl;
+    //else
+    //    cerr << "Error: File reading failed!" << endl;
+    scan.close();
+}
