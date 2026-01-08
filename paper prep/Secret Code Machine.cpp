a907af7dcb427a02ac8337a310560099359cd630
hammadai <hammadshakeel61@gmail.com>
2026-01-08 19:19:48 +0500

    Create Secret Code Machine.cpp

---
+++ paper prep/Secret Code Machine.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int generateCode(int x)
+{
+    int y=1;
+    if (x!=0){
+        for (int i=1; i<=x;i++)
+        {
+            y*=i;// factorial
+        }
+    }
+      return y;
+}
+
+int main()
+{
+    int x;
+    cout<<"enter the number to get a secret code ";
+    cin>>x;
+    int (*codeptr)(int x);
+    codeptr=generateCode;
+    cout<<"your secret code is "<<codeptr(x)<<endl;
+
+
+    return 0;
+}
