3bf038bd5768ad3c11a0f2a08b95a05280cad498
hammadai <hammadshakeel61@gmail.com>
2026-01-10 19:23:04 +0500

    Create 0.1.cpp

---
+++ FINAL PAPER/0.1.cpp
@@ -0,0 +1,38 @@
+struct Node {
+    int rating;
+    std::string name;
+    long long hp; // use long long to be safe
+    Node *left, *right;
+    int sz; // subtree size (for KTH and RANK)
+    Node(int r, std::string n, long long h)
+    : rating(r), name(n), hp(h), left(nullptr), right(nullptr), sz(1) {}
+};//root = deletion(root,)
+Node* deleteN(Node* temproot,Node* node){
+    if (temproot == nullptr){
+        return nullptr;// temproot return karo ya kuch bhi hai wo khali hi deletion not possible 
+    }
+    if(node->left == nullptr && node->right == nullptr){//agar bacghe nai hy tuo bas dlete or retun null
+        delete node;
+        return nullptr;
+    }
+    else if(node->left != nullptr && node->right != nullptr){//dono bache hay
+        Node* succ = findsuccessor(root,node->val);//nasdeek bacha find karo
+        Node* succPar = parent(succ);//osko baap ko dhondo
+        if(succ->right != nullptr){// agar oska right wala bacha hai 
+            succPar->left = succ->right;//tuo iske baap ko iska bacha pakra do
+        succ->left = node->left;//ab nasdek bache ne hameri delete hone wale node ke bacho ko adopt karlia
+        succ->right = node->right;
+        delete node;//or wo khud mar gaya
+        return succ;//ye reurn gogaya for correct place ment 
+    }
+    else if(node->left != nullptr){
+        Node* temp = node->left;
+        delete node;
+        return temp;
+    }
+    else if(node->right != nullptr){
+        Node* temp = node->right;
+        delete node;
+        return temp;
+    }
+}
